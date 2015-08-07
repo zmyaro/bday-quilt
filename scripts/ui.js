ea042e65fb591615537c66472914ba5e03711247
Zachary Yaro <zmyaro@gmail.com>
2015-08-06 22:47:24 -0400

    Update color previews and save settings to localStorage

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,11 +1,15 @@
+'use strict';
+
+var dateParseRegex = /[0-9]*([0-9]{2})-([0-9]{2})-([0-9]{2})/;
+
 window.onload = function () {
 	// Set up input event listeners.
 	document.getElementById('generateBtn').onclick = generateQuilt;
 	document.getElementById('addBdayBtn').onclick = addBday;
-	document.getElementById('colorMode').onchange = updateColors;
+	document.getElementById('colorMode').onchange = handleColorModeChange;
 	
-	// Add one birthday entry to start.
-	addBday();
+	// Load date saved in localStorage.
+	loadStoredData();
 };
 
 /**
@@ -24,6 +28,7 @@ function addBday() {
 	newDatePicker.type = 'date';
 	newDatePicker.required = true;
 	newDatePicker.placeholder = 'YYYY-MM-DD';
+	newDatePicker.oninput = handleDateChange;
 	newListItem.appendChild(newDatePicker);
 	
 	newDeleteBtn.innerHTML = '&times';
@@ -38,19 +43,76 @@ function addBday() {
  */
 function deleteBday() {
 	document.getElementById('bdayList').removeChild(this.parentElement);
+	updateStoredDates();
+}
+
+function handleColorModeChange() {
+	// Save the selected mode to localStorage.
+	localStorage[APP_PREFIX + 'colorMode'] = this.value;
+	updateColors();
+}
+
+function handleDateChange() {
+console.log(this.value);
+	updateStoredDates();
+	updateColors();
 }
 
 /**
- * Update the color previews when the color mode is changed.
+ * Update the color previews
  */
 function updateColors() {
-	var mode = this.value,
+	var mode = document.getElementById('colorMode').value,
 		bdayItems = document.getElementById('bdayList').getElementsByTagName('li');
 	
 	// Convert bdayItems to an Array.
 	bdayItems = Array.prototype.slice.call(bdayItems);
 	
 	bdayItems.forEach(function (item) {
-		// TODO: Update the color previews
+		var date = dateParseRegex.exec(item.getElementsByClassName('dateInput')[0].value),
+			preview = item.getElementsByClassName('colorPreview')[0];
+
+		// If there is no valid date, skip that item.
+		if (!date) {
+			return;
+		}
+		switch (mode) {
+			case 'ymdDec':
+				preview.style.backgroundColor =
+					'#' + date[1] +
+					date[2] +
+					date[3];
+				break;
+			case 'ymdHex':
+				preview.style.backgroundColor =
+					'#' + parseInt(date[1]).toString(16) +
+					parseInt(date[2]).toString(16) +
+					parseInt(date[2]).toString(16);
+				break;
+			case 'dmyDec':
+				preview.style.backgroundColor =
+					'#' + date[3] +
+					date[2] +
+					date[1];
+				break;
+			case 'dmyHex':
+				preview.style.backgroundColor =
+					'#' + parseInt(date[3]).toString(16) +
+					parseInt(date[2]).toString(16) +
+					parseInt(date[1]).toString(16);
+				break;
+			case 'mdyDec':
+				preview.style.backgroundColor =
+					'#' + date[2] +
+					date[1] +
+					date[3];
+				break;
+			case 'mdyHex':
+				preview.style.backgroundColor =
+					'#' + parseInt(date[2]).toString(16) +
+					parseInt(date[1]).toString(16) +
+					parseInt(date[3]).toString(16);
+				break;
+		}
 	});
 }
